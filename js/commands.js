2d32a7ffcccf9c999c9c7c00ac53046815ca1741
Paul Rouget <me@paulrouget.com>
2014-12-11 11:21:33 +0100

    deck controller

--- js/commands.js
+++ js/commands.js
@@ -1,59 +1,56 @@
 define([
-  'js/browser'
-], function(gBrowser) {
-"use strict";
+  'js/iframedeck'
+], function(IframeDeck) {
 
-const Cmds = {
-  goBack: function() {
-    if (gBrowser.selectedTab.hasIframe()) {
-      gBrowser.selectedTab.iframe.goBack();
-    }
-  },
-  goForward: function() {
-    if (gBrowser.selectedTab.hasIframe()) {
-      gBrowser.selectedTab.iframe.goForward();
-    }
-  },
-  reload: function() {
-    if (gBrowser.selectedTab.hasIframe()) {
-      gBrowser.selectedTab.iframe.reload();
-    }
-  },
-  stop: function() {
-    if (gBrowser.selectedTab.hasIframe()) {
-      gBrowser.selectedTab.iframe.stop();
-    }
-  },
-  createNewTab: function(url) {
-      gBrowser.addTab(url, true);
-  },
-  selectNextTab: function() {
-      gBrowser.selectNextTab();
-  },
-  selectPreviousTab: function() {
-    gBrowser.selectPreviousTab();
-  },
-  focusURLBar: function() {
-    document.querySelector(".urlinput").focus();
-    document.querySelector(".urlinput").select();
-  },
-  focusSearchBar: function() {
-    document.querySelector(".searchinput").focus();
-    document.querySelector(".searchinput").select();
-  },
-  closeTab: function() {
-    gBrowser.closeTab(gBrowser.selectedTab);
-  },
-  zoomIn: function() {
-    gBrowser.selectedTab.zoomIn();
-  },
-  zoomOut: function() {
-    gBrowser.selectedTab.zoomOut();
-  },
-  resetZoom: function() {
-    gBrowser.selectedTab.resetZoom();
-  },
-}
+  "use strict";
 
-return Cmds;
+  const Cmds = {
+    goBack: function() {
+      IframeDeck.getSelected().goBack();
+    },
+    goForward: function() {
+      IframeDeck.getSelected().goForward();
+    },
+    reload: function() {
+      IframeDeck.getSelected().reload();
+    },
+    stop: function() {
+      IframeDeck.getSelected().stop();
+    },
+    createNewTab: function(url) {
+      IframeDeck.add({url:url,select:true});
+    },
+    selectNextTab: function() {
+      IframeDeck.selectNext();
+    },
+    selectPreviousTab: function() {
+      IframeDeck.selectPrevious();
+    },
+    focusURLBar: function() {
+      document.querySelector(".urlinput").focus();
+      document.querySelector(".urlinput").select();
+    },
+    focusSearchBar: function() {
+      document.querySelector(".searchinput").focus();
+      document.querySelector(".searchinput").select();
+    },
+    closeTab: function() {
+      IframeDeck.remove(IframeDeck.getSelected());
+    },
+    zoomIn: function() {
+      // FIXME:
+      // gBrowser.selectedTab.zoomIn();
+    },
+    zoomOut: function() {
+      // FIXME:
+      // gBrowser.selectedTab.zoomOut();
+    },
+    resetZoom: function() {
+      // FIXME:
+      // gBrowser.selectedTab.resetZoom();
+    },
+  }
+
+  return Cmds;
+
 });
---
+++ js/iframedeck.js
@@ -0,0 +1,140 @@
+define(["js/eventemitter"], function(EventEmitter) {
+
+  "use strict";
+
+  HTMLIFrameElement.prototype.show = function() {
+    this.removeAttribute("hidden");
+    if (window.IS_PRIVILEGED) {
+      this.setVisible(true);
+    }
+  }
+
+  HTMLIFrameElement.prototype.hide = function() {
+    this.setAttribute("hidden", "true");
+    if (window.IS_PRIVILEGED) {
+      this.setVisible(false);
+    }
+  }
+
+  let _iframeArray = [];
+  let _selectIndex = -1;
+
+  let _iframeParent = document.querySelector(".iframes");
+
+  const IframeDeck = {
+
+    add: function(options={}) {
+      let iframe = document.createElement("iframe");
+      iframe.setAttribute("mozbrowser", "true");
+      iframe.setAttribute("flex", "1");
+      iframe.setAttribute("remote", "true");
+
+      _iframeParent.appendChild(iframe);
+      _iframeArray.push(iframe);
+
+      if (options.select || _selectIndex < 0) {
+        this.select(iframe);
+      } else {
+        iframe.hide();
+      }
+
+      if (options.url) {
+        if (window.IS_PRIVILEGED) {
+          iframe.src = options.url;
+        } else {
+          iframe.src = "data:," + options.url;
+        }
+      }
+
+      this.emit("add", {
+        iframe: iframe,
+        index: _iframeArray.length - 1
+      });
+
+      return iframe;
+    },
+
+    remove: function(iframe) {
+      let index = _iframeArray.indexOf(iframe);
+      if (index < 0) {
+        throw new Error("Unknown iframe");
+      }
+
+      if (_iframeArray.length == 1) {
+        throw new Error("Deck has only one iframe");
+      }
+
+      if (index == _selectIndex) {
+        let newSelectIndex;
+        if (index == _iframeArray.length - 1) {
+          newSelectIndex = index - 1;
+        } else {
+          newSelectIndex = index + 1;
+        }
+        this.select(_iframeArray[newSelectIndex]);
+      }
+
+      _iframeArray.splice(index, 1);
+      iframe.remove();
+
+      this.emit("remove", {
+        iframe: iframe
+      });
+    },
+
+    select: function(iframe) {
+      let index = _iframeArray.indexOf(iframe);
+      if (index < 0) {
+        throw new Error("Unknown iframe");
+      }
+
+      if (_selectIndex > -1) {
+        let selectedIframe = _iframeArray[_selectIndex];
+        selectedIframe.hide();
+      }
+
+      _selectIndex = index;
+      iframe.show();
+
+      this.emit("select", {
+        iframe: iframe,
+        index: index
+      });
+    },
+
+    selectNext: function() {
+      let newSelectIndex = _selectIndex + 1;
+      if (newSelectIndex == _iframeArray.length) {
+        newSelectIndex = 0;
+      }
+      this.select(_iframeArray[newSelectIndex]);
+    },
+
+    selectPrevious: function() {
+      let newSelectIndex = _selectIndex - 1;
+      if (newSelectIndex < 0) {
+        newSelectIndex = _iframeArray.length - 1;
+      }
+      this.select(_iframeArray[newSelectIndex]);
+    },
+
+    getSelected: function() {
+      return _iframeArray[_selectIndex];
+    },
+
+    getCount: function() {
+      return _iframeArray.length;
+    },
+  }
+
+  IframeDeck[Symbol.iterator] = function*() {
+    for (let iframe of _iframeArray) {
+      yield iframe;
+    }
+  }
+
+  EventEmitter.decorate(IframeDeck);
+  IframeDeck.add({url: "http://medium.com"});
+
+  return IframeDeck;
+});
